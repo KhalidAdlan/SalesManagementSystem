d47e547ce0ed36ef7ac5c751ad144fb74b89dec6
Ateeq1999 <ateeq@git.com>
2019-12-14 18:12:21 +0200

    edit the AppUserscontroller

--- app/AppUser.php
+++ app/AppUser.php
@@ -64,24 +64,4 @@ class AppUser extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
-
-     /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    // public function getJWTIdentifier()
-    // {
-    //     return $this->getKey();
-    // }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    // public function getJWTCustomClaims()
-    // {
-    //     return [];
-    // }
 }
