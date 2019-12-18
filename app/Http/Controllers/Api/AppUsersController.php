0b82f9924d26c170e4581453325a3c7839db161f
Khalid Adlan <khalid95adlan@gmail.com>
2019-12-18 17:03:04 +0200

    Add password encryption before authentication

--- app/Http/Controllers/Api/AppUsersController.php
+++ app/Http/Controllers/Api/AppUsersController.php
@@ -13,7 +13,7 @@ class AppUsersController extends Controller
     public $successStatus = 200;
 
     public function login(){ 
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => bcrypt(request('password')]))){ 
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->accessToken; 
             return response()->json(['success' => $success], $this->successStatus); 
